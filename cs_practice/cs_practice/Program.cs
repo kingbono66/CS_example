5a11021363dc3d80ee562cedd7e6061d21d37a16
종근 오 <kyunggioh@hanmail.net>
2020-06-11 19:08:09 +0900

    새로 만듦

---
+++ cs_practice/cs_practice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace cs_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("hello");
+
+
+
+        }
+    }
+}
