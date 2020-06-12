b25fdc92807e7ee4299c6401cc36acdece36b917
academy <Administrator@G-07>
2020-06-12 12:51:04 +0900

    연산자

--- cs_practice/cs_practice/Program.cs
+++ cs_practice/cs_practice/Program.cs
@@ -24,15 +24,48 @@ namespace cs_practice
             */
 
             #region 관계연산자
-            WriteLine($"3 > 4 : {3 > 4}");
-            WriteLine($"3 < 4 : {3 < 4}");
-            WriteLine($"3 >= 4 : {3 >= 4}");
-            WriteLine($"3 <= 4 : {3 <= 4}");
-            WriteLine($"3 == 4 : {3 == 4}");
-            WriteLine($"3 != 4 : {3 != 4}");
+            //WriteLine($"3 > 4 : {3 > 4}");
+            //WriteLine($"3 < 4 : {3 < 4}");
+            //WriteLine($"3 >= 4 : {3 >= 4}");
+            //WriteLine($"3 <= 4 : {3 <= 4}");
+            //WriteLine($"3 == 4 : {3 == 4}");
+            //WriteLine($"3 != 4 : {3 != 4}");
+            #endregion
+            #region 관계논리연산자
+            //WriteLine($"&&");
+            //WriteLine($"1 > 0 && 4 < 5 : {1 > 0 && 4 < 5}");
+            //WriteLine($"1 > 0 && 4 > 5 : {1 > 0 && 4 > 5}");
+            //WriteLine($"1 == 0 && 4 > 5 : {1 == 0 && 4 > 5}");
+            //WriteLine($"1 == 0 && 4 < 5 : {1 == 0 && 4 < 5}");
 
+            //WriteLine($"||");
+            //WriteLine($"1 > 0 || 4 < 5 : {1 > 0 || 4 < 5}");
+            //WriteLine($"1 > 0 || 4 > 5 : {1 > 0 || 4 > 5}");
+            //WriteLine($"1 == 0 || 4 > 5 : {1 == 0 || 4 > 5}");
+            //WriteLine($"1 == 0 || 4 < 5 : {1 == 0 || 4 < 5}");
 
             #endregion
+            int a = 100;
+            a += 90;
+            WriteLine(a);
+            a -= 80;
+            WriteLine(a);
+            a *= 70;
+            WriteLine(a);
+            a /= 60;
+            WriteLine(a);
+            a %= 50;
+            WriteLine(a);
+            a &= 40;
+            WriteLine(a);
+            a |= 30;
+            WriteLine(a);
+            a ^= 20;
+            WriteLine(a);
+            a <<= 10;
+            WriteLine(a);
+            a >>= 1;
+            WriteLine(a);
         }
     }
 }
