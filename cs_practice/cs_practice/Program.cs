2fca56426664ee6a474534987a4013237e3fc7be
academy <Administrator@G-07>
2020-06-19 12:42:15 +0900

    셋커서포지션, 슬립

--- cs_practice/cs_practice/Program.cs
+++ cs_practice/cs_practice/Program.cs
@@ -12,85 +12,24 @@ namespace cs_practice
     {
         static void Main(string[] args)
         {
-            int i = 0;
-            int k = 0;
-            int j = 0;
-
-            i = 1;
-            while(i <= 201 )
+            for(int i = 10; i < 15; i++)
             {
-                WriteLine(i);
-                i += 4;
-            }
-
-            for( i = 1; i <= 9; i++ )
-            {
-                for(k = 1; k <= 9; k++)
-                    Write(i*k +"\t");
-                WriteLine("");
-            }
-
-            for( i = 0; i < 5; i++)
-            {
-                for( k = 0; k <= i; k++)
-                    Write("*");
-                WriteLine("");
-            }
-
-
-            WriteLine("111111111");
-            for (i = 0; i < 5; i++)
-            {
-                for (k = 0; k < (5 - i); k++)
-                    Write("*");
-                WriteLine("");
-            }
-
-
-            WriteLine("22222222222");
-            for (i = 0; i < 5; i++)
-            {
-                for (k = 0; k < 5; k++)
+                for(int j = 4; j < 9; j++)
                 {
-                    if (i < (4-k))
-                        Write(" ");
-                    else
-                        Write("*");
+                    SetCursorPosition(i, j);
+                    if(i==10||i==14||j==4||j==8)
+                        WriteLine("*");
                 }
-                WriteLine("");
             }
 
-
-            WriteLine("333333");
-            for (i = 0; i < 5; i++)
-            {
-                for (j = 0; j < i; j++)
-                    Write(" ");
-                for (k = 0; k < (5-i); k++)
-                    Write("*");
-                WriteLine("");
-            }
+            WriteLine("*");
+            System.Threading.Thread.Sleep(2000);
+            WriteLine("*");
 
 
-            WriteLine("4444444");
-            for (i = 0; i < 4; i++)
-            {
-                for (j = 0; j < (3-i); j++)
-                    Write(" ");
-                for (k = 0; k < (i*2+1); k++)
-                    Write("*");
-                WriteLine("");
-            }
 
-            WriteLine("555555");
-            for (i = 0; i < 7; i++)
-            {
-                for (j = 0; i<4?(j<i):(j<(6-i)); j++)
-                    Write(" ");
-                for (k = 0; i<4?(k<(4-i)):(k<(i-2)); k++)
-                    Write("* ");
-                WriteLine("");
-            }
+            SetCursorPosition(0, 20);
+            WriteLine("");
         }
     }
 }
