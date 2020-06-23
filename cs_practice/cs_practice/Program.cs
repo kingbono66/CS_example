20bfee270a08a7708348ac74b139ebeeb566afac
academy <Administrator@G-07>
2020-06-23 11:32:42 +0900

    .

--- cs_practice/cs_practice/Program.cs
+++ cs_practice/cs_practice/Program.cs
@@ -13,24 +13,37 @@ namespace cs_practice
                
         static void Main(string[] args)
         {
-            int a = 3, b = 5;
+            int a = 123, b = 8;
 
             WriteLine(Plus( a, b));
             WriteLine(Minus(a, b));
             WriteLine(Multi(a, b));
+            WriteLine(divide(a, b));
 
         }
         static int Plus(int x, int y)
         {
+            WriteLine("더하기 연산");
             return x+y;
         }
         static int Minus(int x, int y)
         {
+            WriteLine("빼기 연산");
             return x-y;
         }
         static int Multi(int x, int y)
         {
+            WriteLine("곱하기 연산");
             return x*y;
+        }
+        static int divide(int x, int y)
+        {
+            WriteLine("나누기 연산");
+            int result = 0;
+            if( y != 0 )
+                result = x / y;
+
+            return result;
         }
     }
 }
