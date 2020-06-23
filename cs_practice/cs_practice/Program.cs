8d24009197e52b8433441a77d12b6fae552250e6
academy <Administrator@G-07>
2020-06-23 12:59:23 +0900

    .

--- cs_practice/cs_practice/Program.cs
+++ cs_practice/cs_practice/Program.cs
@@ -13,37 +13,78 @@ namespace cs_practice
                
         static void Main(string[] args)
         {
-            int a = 123, b = 8;
+            //int a = 30, b = 8;
+            //int q = 0;
+            //int r = 0;
 
-            WriteLine(Plus( a, b));
-            WriteLine(Minus(a, b));
-            WriteLine(Multi(a, b));
-            WriteLine(divide(a, b));
+
+            /*
+            WriteLine($"{ a },{ b }");
+            Swap(ref a, ref b);
+            WriteLine($"{ a },{ b }");
+            */
+
+            //WriteLine($"{ a },{ b }");
+            //Divide(a, b, out q, out r);
+            //WriteLine($"{ q },{ r }");
+
+            //WriteLine(Plus(1, 2));
+            //WriteLine(Plus(3.14f, 2.5f));
+            //WriteLine(Plus(1, 2, 3));
+
+            WriteLine(Sum(1, 2, 3, 4, 5));
+            WriteLine(Sum(13, 52));
 
         }
+
+        static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        static void Divide(int a, int b, out int q, out int r)
+        {
+            //if(b!=0)
+            {
+                q = a / b;
+                r = a % b;
+            }            
+        }
+
         static int Plus(int x, int y)
         {
-            WriteLine("더하기 연산");
-            return x+y;
+            return x + y;
+        }
+        static float Plus(float x, float y)
+        {
+            return x + y;
+        }
+        static int Plus(int x, int y, int z)
+        {
+            return x + y + z;
         }
         static int Minus(int x, int y)
         {
-            WriteLine("빼기 연산");
-            return x-y;
+            return x - y;
         }
-        static int Multi(int x, int y)
+        static float Minus(float x, float y)
         {
-            WriteLine("곱하기 연산");
-            return x*y;
+            return x - y;
         }
-        static int divide(int x, int y)
+        static int Minus(int x, int y, int z)
         {
-            WriteLine("나누기 연산");
+            return x - y - z;
+        }
+        static int Sum(params int[] num)
+        {
             int result = 0;
-            if( y != 0 )
-                result = x / y;
-
+            for(int i = 0; i < num.Length; i++)
+            {
+                result += num[i];
+            }
             return result;
         }
+
     }
 }
