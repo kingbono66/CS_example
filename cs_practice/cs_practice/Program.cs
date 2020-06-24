f013179e23c7ddc007cd3f703e0fc62ec94f3c40
종근 오 <kyunggioh@hanmail.net>
2020-06-24 16:23:18 +0900

    .

--- cs_practice/cs_practice/Program.cs
+++ cs_practice/cs_practice/Program.cs
@@ -13,54 +13,64 @@ namespace cs_practice
 
         static void Main(string[] args)
         {
-            CursorVisible = false;
-            //선언부
-            int x = 5, y = 2;
+            int maxPoint = 3;
+            int baseXPosition = 5;
+            int baseYPosition = 4;
+            string character = "';,,;'";
+            string pointChar = "*";
+            int windowSizeX = 100;
+            int windowSizeY = 40;
+            int point1XPosition = 0;
+            int point1YPosition = 0;
+            int point2XPosition = 0;
+            int point2YPosition = 0;
+            int point3XPosition = 0;
+            int point3YPosition = 0;
 
 
-
-            //실행부
-            while (true)
-            {
-                //초기화
-                //Clear();
-
-                //위치세팅
-                SetCursorPosition(x, y);
-
-                //출력
-                Write("^^");
-
-                //키보드입력
-                ConsoleKey key = ReadKey(true).Key;
-                CharacterClear(x, y);
-
-                switch (key)
-                {
-                    case ConsoleKey.UpArrow:
-                        y--;
-                        break;
-                    case ConsoleKey.DownArrow:
-                        y++;
-                        break;
-                    case ConsoleKey.LeftArrow:
-                        x--;
-                        break;
-                    case ConsoleKey.RightArrow:
-                        x++;
-                        break;
-                }
-                if (x < 0) x = 0;
-                if (y < 0) y = 0;
-
-            }
+            
+            InitializeGame(windowSizeX, windowSizeY, ref point1XPosition, ref point1YPosition, 
+                ref point2XPosition, ref point2YPosition, ref point3XPosition, ref point3YPosition);
+            printIntro();
+            //PlayGame(maxPoint, baseXPosition, baseYPosition, character, point);
+            printEnding();
         }
 
-        static void CharacterClear(int x, int y)
+        private static void InitializeGame(int windowSizeX, int windowSizeY, ref int point1XPosition, ref int point1YPosition, 
+                                    ref int point2XPosition, ref int point2YPosition, ref int point3XPosition, ref int point3YPosition)
         {
-            SetCursorPosition(x, y);
-            Write("  ");
+            Random random = new Random();
+
+            CursorVisible = false;
+            SetWindowSize(windowSizeX, windowSizeY);
+            point1XPosition = random.Next(5, 95);
+            point1YPosition = random.Next(1, 40);
+            point2XPosition = random.Next(5, 95);
+            point2YPosition = random.Next(1, 40);
+            point3XPosition = random.Next(5, 95);
+            point3YPosition = random.Next(1, 40);
+
         }
+
+        private static void PlayGame(int maxPoint, int baseXPosition, int baseYPosition, string character, string point)
+        {
+            
+        }
+
+        private static void printEnding()
+        {
+            WriteLine("성공!!! 축하합니다");
+        }
+
+       
+        private static void printIntro()
+        {
+            WriteLine("팩맨 시작합니다");
+            WriteLine("아무키나 누르세요");
+            ReadKey();
+        }
+
+
 
     }
 }
