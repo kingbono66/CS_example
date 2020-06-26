5f9bc6ec98640194cdd05250852baaa1f4da9f40
종근 오 <kyunggioh@hanmail.net>
2020-06-26 17:33:41 +0900

    .

--- cs_practice/cs_practice/Program.cs
+++ cs_practice/cs_practice/Program.cs
@@ -12,11 +12,35 @@ namespace cs_practice
     class Program
     {
         //야구게임 제한턴수9
+        //int.TryParse(ReadLine(), out scores[i])
         static void Main(string[] args)
         {
-            
+            int windowSizeX = 100;
+            int windowSizeY = 40;
+
+            //초기화 및 설정
 
 
+
+            //플레이
+
+
+
+            //마무리
+
+
+        }
+
+        static int getRangedIntInput(int min, int max)
+        {
+            int intInput = -99999999;
+            while (true)
+            {
+                int.TryParse(ReadLine(), out intInput);
+                if (intInput >= min && intInput <= max)
+                    break;
+            }
+            return intInput;
         }
     }
 }
