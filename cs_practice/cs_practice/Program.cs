b0577539a2472255fbbc425909960415c1f5a4d3
academy <Administrator@G-07>
2020-06-26 12:39:40 +0900

    .

--- cs_practice/cs_practice/Program.cs
+++ cs_practice/cs_practice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -13,89 +14,133 @@ namespace cs_practice
 
         static void Main(string[] args)
         {
-            Random random = new Random();
-
             /*
-            int[] nums = new int[3];
-            nums[0] = 3;
-            nums[1] = 4;
-            nums[2] = -2;
-
-            WriteLine(nums[0]);
-            WriteLine(nums[1]);
-            WriteLine(nums[2]);
-
-            int[] students = new int[7] { 10, 100, 55, 40, 20, 88, 66 };
-            for (int i = 0; i < students.Length; i++)
-                WriteLine(students[i]);
-
-            WriteLine("foreach");
-            foreach( int s in students)
-                WriteLine(s);
-
+            int[] scores = new int[10];
+            bool[] isPass = new bool[10];
             int sum = 0;
-            for (int i = 0; i < students.Length; i++)
-                sum += students[i];
-            WriteLine(sum);
-            WriteLine(sum / students.Length);
 
-            int[] scores = new int[30];
-            for (int i = 0; i < scores.Length; i++)
+            for( int i = 0; i < scores.Length; i++)
             {
-                scores[i] = random.Next(0, 101);
-                Write(scores[i] + "\t");
+                Write($"학생{i + 1} : ");
+                //scores[i] = int.Parse(ReadLine());
+                while(!int.TryParse(ReadLine(), out scores[i]))
+                {
+                    Write($"학생{i + 1} : ");
+                }
+                if( scores[i] < 0 || scores[i] > 100)
+                {
+                    i--;
+                    continue;
+                }
+
+                if (scores[i] >= 60)
+                    isPass[i] = true;
+                else
+                    isPass[i] = false;
+                Clear();
             }
-            WriteLine();
-            foreach( int e in scores)
+            foreach(int e in scores)
             {
-                if (e >= 70)
-                    WriteLine(e);
+                sum += e;
+            }
+            WriteLine($"합계 : {sum}\t\t평균 : {((float)sum/scores.Length).ToString("F2")}");
+            WriteLine("합격자");
+            WriteLine("---------------------------");
+            for( int i = 0; i < isPass.Length; i++)
+            {
+                if (isPass[i])
+                    WriteLine("학생 {0,3}", (i + 1));
             }
             */
 
+            /*
+            int[][] jagged = new int[3][];
+            jagged[0] = new int[5] { 1, 2, 3, 4, 5 };
+            jagged[1] = new int[] { 6, 7 };
+            jagged[2] = new int[] { 8 };
 
-            int[] koreanScores = new int[10];
-            int[] englishScores = new int[10];
-            int[] mathScores = new int[10];
-
-            for (int i = 0; i < koreanScores.Length; i++)
+            for(int i = 0; i < jagged.Length; i++)
             {
-                koreanScores[i] = random.Next(0, 101);
-                englishScores[i] = random.Next(0, 101);
-                mathScores[i] = random.Next(0, 101);
-            }
-
-            for (int i = 0; i < koreanScores.Length; i++)
-            {
-                WriteLine($"학생{i + 1} 국어: {koreanScores[i]}, 영어:{englishScores[i]}, 수학:{mathScores[i]}, " +
-                    $"합계:{koreanScores[i]+ englishScores[i]+ mathScores[i] }, 평균점{(koreanScores[i] + englishScores[i] + mathScores[i]) /3}");
-                //Write("학생{0,4}", i); 자릿수지정
-            }
-
-            int[] arr1 = new int[3] { 1, 2, 3 };
-            int[] arr2 = new int[] { 1, 2, 3 };
-            int[] arr3 = { 1, 2, 3 };
-
-            int[,] scores = new int[4, 3]
-            {
-                {1,2,3 },
-                {4,5,6 },
-                {7,8,9 },
-                {10,11,12 }
-            };
-            for(int i = 0; i < scores.GetLength(0); i++)
-            {
-                for (int j = 0; j < scores.GetLength(1); j++)
-                    WriteLine($"{scores[i, j]} ");
+                foreach(int e in jagged[i])
+                {
+                    WriteLine(e);
+                }
                 WriteLine();
             }
+            */
 
+            /*
+            ArrayList list = new ArrayList();
+            for (int i = 0; i < 5; i++)
+                list.Add(i);
+            foreach (object obj in list)
+                Write($"{obj} ");
+            WriteLine();
+
+            list.RemoveAt(2);
+            foreach (object obj in list)
+                Write($"{obj} ");
+            WriteLine();
+
+            list.Insert(2, 2);
+            foreach (object obj in list)
+                Write($"{obj} ");
+            WriteLine();
+
+            list.Add("abc");
+            list.Add("def");
+
+            for (int i = 0; i < list.Count; i++)
+                Write($"{list[i]} ");
+            WriteLine();
+            */
+
+            /*
+            Stack stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+            while(stack.Count > 0)
+            {
+                WriteLine(stack.Pop());
+            }
+            */
+
+            /*
+            Queue queue = new Queue();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+            while (queue.Count > 0)
+            {
+                WriteLine(queue.Dequeue());
+            }
+            */
+
+            Hashtable ht = new Hashtable();
+            ht["하나"] = "one";
+            ht["둘"] = "two";
+            ht["셋"] = "three";
+            ht["넷"] = "four";
+            ht["다섯"] = "five";
+
+            WriteLine(ht["하나"]);
+            WriteLine(ht["둘"]);
+            WriteLine(ht["셋"]);
+            WriteLine(ht["넷"]);
+            WriteLine(ht["다섯"] + "\n");
+            foreach(object o in ht.Values)
+            {
+                WriteLine(o);
+            }
 
 
 
         }
-
-
-
     }
 }
